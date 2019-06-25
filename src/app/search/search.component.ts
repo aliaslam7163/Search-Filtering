a488f9f7523171b4dbab7f0beb87d1e1da256568
aliaslam7163 <aliaslam7163@gmail.com>
2019-06-25 12:09:07 -0700

    2 files modified

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray} from '@angular/forms';
 import { of } from 'rxjs';
 import { Filter } from '../models/filter.model';
@@ -9,12 +9,13 @@ import { Filter } from '../models/filter.model';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  //@ViewChildren() filterSelector2: QueryList;
   availableSearch:any[] = [
     {outputModel:'',label:'Name',code:'name',searchField:{type:'text',label:'Name',code:'name'}},
-    {outputModel:'',label:'Carrier Code',code:'code',searchField:{type:'text',label:'Carrier Code',code:'code'}},
-    {outputModel:'',label:'Defined By',code:'definedby',searchField:{type:'text',label:'Defined By',code:'definedby'}},
-    {outputModel:'',label:'Status',code:'status',options:['active','inactive'],searchField:{type:'select',label:'Status',code:'status'}},
-    {outputModel:'',label:'Carriers',code:'carrier',options:['UPS','DHL','Fedex'],searchField:{type:'multi',label:'Carriers',code:'carrier'}}
+    {outputModel:'',label:'Carrier Code',code:'code',searchField:{type:'TEXT',label:'Carrier Code',code:'code'}},
+    {outputModel:'',label:'Defined By',code:'definedby',searchField:{type:'TEXT',label:'Defined By',code:'definedby'}},
+    {outputModel:'',label:'Status',code:'status',options:['active','inactive'],searchField:{type:'SELECT',label:'Status',code:'status'}},
+    {outputModel:'',label:'Carriers',code:'carrier',options:['UPS','DHL','Fedex'],searchField:{type:'MULTI',label:'Carriers',code:'carrier'}}
   ];
   searchFields = ['Name','Carrier','Status','DefinedBy'];
   availableFields:any[] = ['Name','Carrier','Status','DefinedBy'];
@@ -24,6 +25,8 @@ export class SearchComponent implements OnInit {
   constructor(private fb:FormBuilder) {}
 
   ngOnInit() {
+    //Lots of processing needs to be done before this
+    //Grabbing filter model from server
     let filter:Partial<Filter> = {
       label:this.availableSearch[0].label,
       code:this.availableSearch[0].code,
@@ -35,16 +38,22 @@ export class SearchComponent implements OnInit {
 
   addFieldsDynamic(filterUse,value){
     if(this.dynamicInUse.length < this.availableFields.length){
-        let temp = this.availableSearch.filter((element) => {
+        this.availableSearch = this.availableSearch.filter((element) => {
         return this.dynamicInUse.filter((filtersUsed) => {
           return filtersUsed.label == element.label;
         }).length == 0;
       });
-      console.log(temp.map((obj) => {return obj.label}));
+      console.log(this.availableSearch.map((obj) => {return obj}));
 
       //console.log(temp);
       this.dynamicInUse.forEach((element) => element.disabled=true);
-      //this.dynamicInUse.push(newObj);
+      let filter:Partial<Filter> = {
+      label:this.availableSearch[0].label,
+      code:this.availableSearch[0].code,
+      type:this.availableSearch[0].searchField.type,
+      disabled:true
+    }
+    this.dynamicInUse.push(filter);
     }
   }
 
@@ -52,7 +61,9 @@ export class SearchComponent implements OnInit {
   setModelDynamic(filterInUse,value){
     //console.log(filterInUse);
     //console.log(value);
-    this.dynamicInUse[this.inUse.length-1].outputModel = value;
+    //this.dynamicInUse[this.inUse.length-1].outputModel = value;
     console.log(this.dynamicInUse);
   }
+
+  grabFilterModel(search:string){}
 }
